9e5e19a7579e430a163a1a51db5081ad68a59592
QuyenLy98 <vanquyen0508@gmail.com>
2023-08-12 15:23:47 +0700

    Update xuli.js

--- public/xuli.js
+++ public/xuli.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:3000');
+var socket = io('https://add-list-socket.onrender.com');
 socket.on('server-send-thongtin', function (data) {
   $('#txthoten').val('');
   $('#txtemail').val('');
